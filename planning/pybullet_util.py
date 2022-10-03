c305f7ca3a4028fcd6417d4a3905546318793c4a
Ondrej Biza <ondrej.biza@gmail.com>
2022-10-03 19:37:51 -0400

    experiments wtih motion planning

---
+++ planning/pybullet_util.py
@@ -0,0 +1,10 @@
+import pybullet as pb
+import numpy as np
+
+
+def get_matrix(pos, rot):
+
+    T = np.eye(4)
+    T[:3, :3] = np.array(pb.getMatrixFromQuaternion(rot)).reshape((3, 3))
+    T[:3, 3] = pos
+    return T
