1d4f7e10823a6953d5844f518f0eb3e1898e8431
Ondrej Biza <ondrej.biza@gmail.com>
2022-10-10 17:04:14 -0400

    Contact points for a mug and a mug tree. Visualizer.

---
+++ show_urdf.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+import pybullet as pb
+import time
+from pybullet_planning.pybullet_tools import utils as pu
+
+
+def main(args):
+
+    pu.connect(use_gui=True)
+    pu.disable_real_time()
+    pu.draw_global_system()
+
+    pb.setPhysicsEngineParameter(enableFileCaching=0)
+
+    while True:
+
+        path = os.path.join("..", args.path)
+        obj = pu.load_model(path, fixed_base=False)    
+        print("Num. joints: {:d}".format(pb.getNumJoints(obj)))
+
+        time.sleep(1)
+        pu.remove_body(obj)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+main(parser.parse_args())
