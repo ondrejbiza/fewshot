fe417fb9b5392f7200ef3b4e435d00d10aed67eb
Dian Wang <wangdian1007@gmail.com>
2023-01-29 15:50:52 -0500

    Success at mug tree.

--- online_isec/moveit_plan_pick_place.py
+++ online_isec/moveit_plan_pick_place.py
@@ -1,22 +1,24 @@
+import argparse
 import numpy as np
+from numpy.typing import NDArray
+import os
 import pickle
 import rospy
 from scipy.spatial.transform import Rotation
-import time
+from typing import Any, Dict, Tuple
 
 from online_isec import constants
-from online_isec.point_cloud_proxy import RealsenseStructurePointCloudProxy
-import online_isec.utils as isec_utils
-import online_isec.utils as isec_utils
 from online_isec import perception
+from online_isec.point_cloud_proxy import RealsenseStructurePointCloudProxy
 from online_isec.ur5 import UR5
 import utils
-from pybullet_planning.pybullet_tools import utils as pu
 
 
-def pick(mug_pc_complete, mug_param, ur5, safe_release: bool=False):
+def pick(
+    mug_pc_complete: NDArray, mug_param: Tuple[NDArray, NDArray, NDArray],
+    ur5: UR5, load_path: str, safe_release: bool=False) -> NDArray:
 
-    with open("data/real_pick_clone.pkl", "rb") as f:
+    with open(load_path, "rb") as f:
         data = pickle.load(f)
         index, target_quat = data["index"], data["quat"]
         target_pos = mug_pc_complete[index]
@@ -55,26 +57,12 @@ def pick(mug_pc_complete, mug_param, ur5, safe_release: bool=False):
     return T_m_to_g
 
 
-def main():
+def place(
+    ur5: UR5, canon_mug: Dict[str, Any], canon_tree: Dict[str, Any],
+    mug_param: Tuple[NDArray, NDArray, NDArray], tree_param: Tuple[NDArray, NDArray],
+    T_m_to_g: NDArray, load_path: str):
 
-    rospy.init_node("moveit_plan_pick_place")
-    pc_proxy = RealsenseStructurePointCloudProxy()
-
-    ur5 = UR5(setup_planning=True)
-    ur5.plan_and_execute_joints_target(ur5.home_joint_values)
-
-    cloud = pc_proxy.get_all()
-    assert cloud is not None
-
-    mug_pc_complete, mug_param, tree_pc_complete, tree_param, canon_mug, canon_tree = perception.mug_tree_perception(
-        pc_proxy, np.array(constants.DESK_CENTER), ur5.tf_proxy, ur5.moveit_scene,
-        add_mug_to_planning_scene=False, add_tree_to_planning_scene=False, rviz_pub=ur5.rviz_pub,
-        mug_save_decomposition=True, close_proxy=True
-    )
-
-    T_m_to_g = pick(mug_pc_complete, mug_param, ur5)
-
-    with open("data/real_place_clone.pkl", "rb") as f:
+    with open(load_path, "rb") as f:
         place_data = pickle.load(f)
 
     knns = place_data["knns"]
@@ -90,21 +78,58 @@ def main():
     targets_tree = tree_orig[target_indices]
 
     T_new_m_to_t, _, _ = utils.best_fit_transform(targets_mug, targets_tree)
-    print("Best fit spatial transform:")
-    print(T_new_m_to_t)
-
     T_t_to_b = utils.pos_rot_to_transform(tree_param[0] + constants.DESK_CENTER, utils.yaw_to_rot(tree_param[1]))
     T_new_m_to_b = np.matmul(T_t_to_b, T_new_m_to_t)
-
     T_g_to_b = np.matmul(T_new_m_to_b, np.linalg.inv(T_m_to_g))
 
     ur5.plan_and_execute_pose_target_2(*utils.transform_to_pos_quat(T_g_to_b))
-    input("Release?")
+
+
+def main(args):
+
+    rospy.init_node("moveit_plan_pick_place")
+    pc_proxy = RealsenseStructurePointCloudProxy()
+
+    ur5 = UR5(setup_planning=True)
+    ur5.plan_and_execute_joints_target(ur5.home_joint_values)
+
+    cloud = pc_proxy.get_all()
+    assert cloud is not None
+
+    mug_pc_complete, mug_param, tree_pc_complete, tree_param, canon_mug, canon_tree = perception.mug_tree_perception(
+        pc_proxy, np.array(constants.DESK_CENTER), ur5.tf_proxy, ur5.moveit_scene,
+        add_mug_to_planning_scene=False, add_tree_to_planning_scene=True, rviz_pub=ur5.rviz_pub,
+        mug_save_decomposition=True, close_proxy=True
+    )
+
+    T_m_to_g = pick(mug_pc_complete, mug_param, ur5, args.pick_load_path + ".pkl")
+    ur5.plan_and_execute_joints_target(ur5.home_joint_values)
+
+    if args.multiple_waypoints:
+        i = 0
+        while True:
+            load_path = args.place_load_path + "_{:d}.pkl".format(i)
+            if not os.path.isfile(load_path):
+                break
+            place(ur5, canon_mug, canon_tree, mug_param, tree_param, T_m_to_g, load_path)
+            i += 1
+    else:
+        load_path = args.place_load_path + ".pkl"
+        if not os.path.isfile(load_path):
+            print("Place clone file not found. Set -m?")
+        else:
+            place(ur5, canon_mug, canon_tree, mug_param, tree_param, T_m_to_g, load_path)
+
+    # input("Release?")
     ur5.gripper.open_gripper()
 
-    input("Reset?")
+    # input("Reset?")
     ur5.plan_and_execute_joints_target(ur5.home_joint_values)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pick-load-path", type=str, default="data/220129_real_pick_clone", help="Postfix added automatically.")
+    parser.add_argument("--place-load-path", type=str, default="data/220129_real_place_clone", help="Postfix added automatically.")
+    parser.add_argument("-m", "--multiple-waypoints", default=False, action="store_true", help="Multiple place waypoint.")
+    main(parser.parse_args())
--- online_isec/save_pick_place.py
+++ online_isec/save_pick_place.py
@@ -8,7 +8,7 @@ from scipy.spatial.transform import Rotation
 import rospy
 import threading
 import time
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Tuple
 
 from online_isec import constants
 from online_isec import perception
@@ -29,31 +29,31 @@ def worker(ur5: UR5, sphere: int, mug: int, data: Dict[str, Any]):
         gripper_pos = gripper_pos - constants.DESK_CENTER
 
         if data["T"] is not None:
+            # Make the mug follow the gripper.
             T_g_to_b = utils.pos_quat_to_transform(gripper_pos, gripper_quat)
             T_m_to_b = np.matmul(T_g_to_b, data["T"])
             m_pos, m_quat = utils.transform_to_pos_quat(T_m_to_b)
             pu.set_pose(mug, (m_pos, m_quat))
 
+        # Mark the gripper with a sphere.
         pu.set_position(sphere, gripper_pos[0], gripper_pos[1], gripper_pos[2])
         time.sleep(0.1)
 
 
-def get_knn_and_deltas(obj, vps):
+def get_knn_and_deltas(obj: NDArray, vps: NDArray, k: int=10) -> Tuple[NDArray, NDArray]:
 
-    k = 10
-    # [n_pairs, n_points, 3]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.scatter(obj[:, 0], obj[:, 1], obj[:, 2], color="red", alpha=0.1)
-    ax.scatter(vps[:, 0], vps[:, 1], vps[:, 2], color="green")
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection="3d")
+    # ax.scatter(obj[:, 0], obj[:, 1], obj[:, 2], color="red", alpha=0.1)
+    # ax.scatter(vps[:, 0], vps[:, 1], vps[:, 2], color="green")
+    # plt.show()
 
     dists = np.sum(np.square(obj[None] - vps[:, None]), axis=-1)
     knn_list = []
     deltas_list = []
 
     for i in range(dists.shape[0]):
+        # Get K closest points, compute relative vectors.
         knn = np.argpartition(dists[i], k)[:k]
         deltas = vps[i: i + 1] - obj[knn]
         knn_list.append(knn)
@@ -64,24 +64,35 @@ def get_knn_and_deltas(obj, vps):
     return knn_list, deltas_list
 
 
-def save_pick_pose(filled_and_transformed_mug: NDArray[np.float32], gripper_pos: NDArray[np.float32],
-                   gripper_rot: NDArray[np.float32], save: bool):
+def save_pick_pose(mug_pc_complete: NDArray[np.float32], mug_param: Tuple[NDArray, NDArray, NDArray],
+                   ur5: UR5, save_path: Optional[bool]=None):
 
-    dist = np.sqrt(np.sum(np.square(filled_and_transformed_mug - gripper_pos), axis=1))
+    gripper_pos, gripper_quat = ur5.get_end_effector_pose()
+    gripper_pos = gripper_pos - constants.DESK_CENTER
+    gripper_rot = np.matmul(
+        utils.yaw_to_rot(mug_param[2]).T,
+        Rotation.from_quat(gripper_quat).as_matrix()
+    )
+    gripper_quat = Rotation.from_matrix(gripper_rot).as_quat()
+
+    dist = np.sqrt(np.sum(np.square(mug_pc_complete - gripper_pos), axis=1))
     index = np.argmin(dist)
 
-    if save:
-        with open("data/real_pick_clone.pkl", "wb") as f:
+    if save_path:
+        with open(save_path, "wb") as f:
             pickle.dump({
                 "index": index,
-                "quat": gripper_rot
+                "quat": gripper_quat
             }, f)
 
 
-def save_place_contact_points(ur5, mug, tree, T_m_to_g, canon_mug, canon_tree, mug_param, tree_param, save: bool):
+def save_place_contact_points(
+    ur5: UR5, mug: int, tree: int, T_m_to_g: NDArray,canon_mug: Dict[str, Any], canon_tree: Dict[str, Any],
+    mug_param: Tuple[NDArray, NDArray, NDArray], tree_param: Tuple[NDArray, NDArray], delta: float=0.1,
+    save_path: Optional[str]=None):
 
     pb.performCollisionDetection()
-    cols = pb.getClosestPoints(mug, tree, 0.01)
+    cols = pb.getClosestPoints(mug, tree, delta)
 
     spheres_mug = []
     spheres_tree = []
@@ -119,8 +130,8 @@ def save_place_contact_points(ur5, mug, tree, T_m_to_g, canon_mug, canon_tree, m
     dist_2 = np.sqrt(np.sum(np.square(canon_tree["canonical_obj"][:, None] - pos_tree_tree_coords[None]), axis=2))  # TODO: is this correct?
     i_2 = np.argmin(dist_2, axis=0).transpose()
 
-    if save:
-        with open("data/real_place_clone.pkl", "wb") as f:
+    if save_path is not None:
+        with open(save_path, "wb") as f:
             pickle.dump({
                 "knns": knns,
                 "deltas": deltas,
@@ -137,10 +148,11 @@ def main(args):
     # ur5.plan_and_execute_joints_target(ur5.home_joint_values)
 
     mug_pc_complete, mug_param, tree_pc_complete, tree_param, canon_mug, canon_tree = perception.mug_tree_perception(
-        pc_proxy, np.array(constants.DESK_CENTER),
-        add_mug_to_planning_scene=False, add_tree_to_planning_scene=False
+        pc_proxy, np.array(constants.DESK_CENTER), add_mug_to_planning_scene=False, add_tree_to_planning_scene=False,
+        mug_save_decomposition=True
     )
 
+    # Setup simulation with what we see in the real world.
     pu.connect(use_gui=True, show_sliders=False)
     pu.set_default_camera(distance=2)
     pu.disable_real_time()
@@ -148,30 +160,21 @@ def main(args):
 
     mug = pu.load_model("../tmp.urdf", pu.Pose(mug_param[1], pu.Euler(yaw=mug_param[2])))
     tree = pu.load_model("../data/real_tree.urdf", pu.Pose(tree_param[0], pu.Euler(yaw=tree_param[1])), fixed_base=True)
-
     point = pu.load_model("../data/sphere.urdf")
+
+    # Continuously update the gripper position in simulation.
     data = {
         "T": None,
         "stop": False,
     }
-
     thread = threading.Thread(target=worker, args=(ur5, point, mug, data))
     thread.start()
+
     input("Close gripper?")
+    ur5.gripper.close_gripper()
+    save_pick_pose(mug_pc_complete, mug_param, ur5, args.pick_save_path + ".pkl")
 
-    gripper_pos, gripper_rot = ur5.get_end_effector_pose()
-    gripper_pos = gripper_pos - constants.DESK_CENTER
-
-    # TODO: We do not account for the mug moving as it is picked.
-    tmp = np.matmul(
-        utils.yaw_to_rot(mug_param[2]).T,
-        Rotation.from_quat(gripper_rot).as_matrix()
-    )
-    gripper_rot = Rotation.from_matrix(tmp).as_quat()
-
-    save_pick_pose(mug_pc_complete, gripper_pos, gripper_rot, args.save)
-
-
+    # Calculate mug to gripper transform. Transmit it to simulation.
     gripper_pos, gripper_rot = ur5.get_end_effector_pose()
     gripper_pos = gripper_pos - constants.DESK_CENTER
     T_g_to_b = utils.pos_quat_to_transform(gripper_pos, gripper_rot)
@@ -179,20 +182,28 @@ def main(args):
     T_m_to_g = np.matmul(np.linalg.inv(T_g_to_b), T_m_to_b)
     data["T"] = T_m_to_g
 
-    ur5.gripper.close_gripper()
+    # Save any number of waypoints.
+    # input("Save place waypoint?")
+    i = 0
+    while True:
+        save_place_contact_points(
+            ur5, mug, tree, T_m_to_g, canon_mug, canon_tree, mug_param, tree_param, save_path=args.place_save_path + "_{:d}.pkl".format(i))
+        tmp = input("Save another? (y/n)")
+        if tmp != "y":
+            break
+        i += 1
 
-    input("Open gripper?")
+    # Reset.
+    # input("Open gripper?")
+    ur5.gripper.open_gripper()
+    # ur5.plan_and_execute_joints_target(ur5.home_joint_values)
 
     data["stop"] = True
     thread.join()
 
-    save_place_contact_points(ur5, mug, tree, T_m_to_g, canon_mug, canon_tree, mug_param, tree_param, args.save)
-
-    ur5.gripper.open_gripper()
-    # ur5.plan_and_execute_joints_target(ur5.home_joint_values)
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--show-perception", default=False, action="store_true")
-parser.add_argument("--save", default=False, action="store_true")
+parser.add_argument("--pick-save-path", type=str, default="data/220129_real_pick_clone", help="Postfix added automatically.")
+parser.add_argument("--place-save-path", type=str, default="data/220129_real_place_clone", help="Postfix added automatically.")
 main(parser.parse_args())
