e8412128a4ef6f0fd102180c594317439de460ca
Ondrej Biza <ondrej.biza@gmail.com>
2023-03-14 19:19:54 -0400

    Figure 4 and more PCAs.

--- scripts/real_world/visualizations/show_pick_warping.py
+++ scripts/real_world/visualizations/show_pick_warping.py
@@ -1,12 +1,13 @@
 import argparse
+import os
 import pickle
 
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Button, Slider
 
-from src import utils
+from src import utils, viz_utils
 
 
 def update_axis(ax, source_obj: NDArray, robotiq_points: NDArray, vmin: float, vmax: float):
@@ -24,7 +25,10 @@ def main(args):
     smin, smax = -2., 2.
     vmin, vmax = -0.3, 0.3
 
-    if args.task == "bowl_on_mug":
+    if args.task == "mug_tree":
+        canon_source = utils.CanonObj.from_pickle("data/230227_ndf_mugs_scale_large_pca_8_dim_alp_0_01.pkl")
+        canon_source.init_scale = 0.7
+    elif args.task == "bowl_on_mug":
         canon_source = utils.CanonObj.from_pickle("data/230227_ndf_bowls_scale_large_pca_8_dim_alp_0_01.pkl")
         canon_source.init_scale = 0.8
     else:
@@ -37,16 +41,16 @@ def main(args):
     pcd = canon_source.to_pcd(utils.ObjParam(latent=np.zeros(canon_source.n_components)))
 
     pos = pcd[index]
-    trans, _, _ = utils.best_fit_transform(pos_robotiq, pos)
-    pos_target = utils.transform_pcd(pos_robotiq, trans)
+    trans, _, _ = utils.best_fit_transform(pos, pos_robotiq)
+    pcd = utils.transform_pcd(pcd, trans)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    update_axis(ax, pcd, pos_target, vmin, vmax)
+    update_axis(ax, pcd, pos_robotiq, vmin, vmax)
 
     slider_axes = []
     z = 0.
-    for _ in range(canon_source.n_components):
+    for _ in range(canon_source.n_components + 1):
         slider_axes.append(fig.add_axes([0.25, z, 0.65, 0.03]))
         z += 0.05
     # we start at the bottom and move up
@@ -55,17 +59,36 @@ def main(args):
     sliders = []
     for i in range(canon_source.n_components):
         sliders.append(Slider(slider_axes[i], "D{:d}".format(i), smin, smax, valinit=0))
+    button = Button(slider_axes[canon_source.n_components], "Save pcd")
 
     def sliders_on_changed(val):
         latents = np.array([[s.val for s in sliders]])
         source_pcd = canon_source.to_pcd(utils.ObjParam(latent=latents, scale=np.ones(3) * canon_source.init_scale))
         pos = source_pcd[index]
-        trans, _, _ = utils.best_fit_transform(pos_robotiq, pos)
-        pos_target = utils.transform_pcd(pos_robotiq, trans)
-        update_axis(ax, source_pcd, pos_target, vmin, vmax)
+        trans, _, _ = utils.best_fit_transform(pos, pos_robotiq)
+        source_pcd = utils.transform_pcd(source_pcd, trans)
+        update_axis(ax, source_pcd, pos_robotiq, vmin, vmax)
+
+    def button_on_changed(val):
+        latents = np.array([[s.val for s in sliders]])
+        source_pcd = canon_source.to_pcd(utils.ObjParam(latent=latents, scale=np.ones(3) * canon_source.init_scale))
+        pos = source_pcd[index]
+        trans, _, _ = utils.best_fit_transform(pos, pos_robotiq)
+        source_pcd = utils.transform_pcd(source_pcd, trans)
+
+        dir_path = "data/warping_figure_4"
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+        i = 1
+        for i in range(1, 1000):
+            file_path = os.path.join(dir_path, f"{i}.pcd")
+            if not os.path.isfile(file_path):
+                break
+        viz_utils.save_o3d_pcd(source_pcd, file_path)
 
     for s in sliders:
         s.on_changed(sliders_on_changed)
+    button.on_clicked(button_on_changed)
 
     plt.show()
 
