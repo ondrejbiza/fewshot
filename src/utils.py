808c2618f9a5d0f888c8587af9c8922af36a192c
Dian Wang <wangdian1007@gmail.com>
2023-03-29 15:59:12 -0400

    Account for trimesh bug.

--- src/utils.py
+++ src/utils.py
@@ -211,7 +211,10 @@ def convex_decomposition(mesh: trimesh.base.Trimesh, save_path: Optional[str]=No
         alpha=0.05, beta=0.05, gamma=0.00125, pca=0, mode=0, maxNumVerticesPerCH=256, minVolumePerCH=0.0001,
         convexhullApproximation=1, oclDeviceID=0
     )
-    print("# convex meshes:", len(convex_meshes))
+    # Bug / undocumented feature in Trimesh :<
+    if not isinstance(convex_meshes, list):
+        convex_meshes = [convex_meshes]
+
     if save_path is not None:
         decomposed_scene = trimesh.scene.Scene()
         for i, convex_mesh in enumerate(convex_meshes):
